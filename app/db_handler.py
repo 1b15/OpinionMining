0ac3721c18f009aa0bd538dbd455263f180dbc14
Georg Reich <georg-reich@gmx.de>
2019-11-09 15:54:03 +0100

    bug fixes

--- app/db_handler.py
+++ app/db_handler.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-users = pd.read_csv('DB/users.csv').set_index('id')
-challenges = pd.read_csv('DB/challenges.csv').set_index('id')
-recipes = pd.read_csv('DB/recipes.csv').set_index('id')
-challengeLikes = pd.read_csv('DB/challengeLikes.csv')
-recipesLikes = pd.read_csv('DB/recipesLikes.csv')
+users = pd.read_csv('app/DB/users.csv').set_index('id')
+challenges = pd.read_csv('app/DB/challenges.csv').set_index('id')
+recipes = pd.read_csv('app/DB/recipes.csv').set_index('id')
+challengeLikes = pd.read_csv('app/DB/challengeLikes.csv')
+recipesLikes = pd.read_csv('app/DB/recipesLikes.csv')
 
 def get_user_profile(user_id):
     user = users.iloc[user_id]
