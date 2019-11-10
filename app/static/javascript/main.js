543aa63d75986656b9a44a4d39a5b01eb4b90da5
Xenomus <38104558+Zernerus@users.noreply.github.com>
2019-11-10 12:15:09 +0100

    Added the "add recipe functions"

--- app/static/javascript/main.js
+++ app/static/javascript/main.js
@@ -7,10 +7,34 @@ function unhoverBackArrow(element) {
   element.setAttribute('src', '/static/img/leftArrowEmpty.svg');
 }
 
-function hoverHeartLike(element) {
-  element.setAttribute('src', '/static/img/HeartLikeFull.svg');
-}
+function like(ID, img){
+  $.ajax({
+			type: 'POST',
+			url: 'challengeLike/'+ID,
+		}).done(function(data) {
+		    var hidden = $('#inputnr'+ID);
+		    if(hidden.val() == 0){
+                hidden.val(1);
+                img.setAttribute('src', '/static/img/HeartLikeFull.svg');
+                var likeScore = document.getElementById('likeCount'+ID);
+                console.log(likeScore);
+                var number = likeScore.innerHTML;
+                number++;
+                likeScore.innerHTML = number;
+            }
+            else{
+                hidden.val(0);
+                img.setAttribute('src', '/static/img/HeartLikeEmpty.svg');
+                var likeScore = document.getElementById('likeCount'+ID);
+                console.log(likeScore);
+                var number = likeScore.innerHTML;
+                number--;
+                likeScore.innerHTML = number;
+            }
+			console.log('done: '+data);
+		}).fail(function(data) {
+			// Aktionen bei einem Fehler
+			console.log('fail: '+data);
+		});
 
-function unhoverHeartLike(element) {
-  element.setAttribute('src', '/static/img/HeartLikeEmpty.svg');
 }
