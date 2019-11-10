e7d7f3df00048e8a1233df4b0e5f1845bdb286f5
Xenomus <38104558+Zernerus@users.noreply.github.com>
2019-11-10 10:53:14 +0100

    Added the "add recipe functions"

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import sys
-from app import app
+from app import app, APP_STATIC
 from flask import render_template, request, redirect, url_for, jsonify
+import os
 
 from app.db_handler import get_user_profile, post_challenge, get_challenges, get_recipes, get_challenge, login
 
@@ -32,11 +33,26 @@ def challenge():
 @app.route('/recipe/<challenge_id>', methods = ['POST'])
 def add_recipe(challenge_id):
     if request.method == 'POST':
-        req_data = request.get_json()
-        #
-        print(req_data)
+
+        image = request.files["image"] #image.fileename ->Name des Bildes
+        print(request.form['comment']) #Freitext
+
+        image.save(os.path.join(app.root_path, 'static', 'img', image.filename)) #Speichere Bild ab
+
     return jsonify(code='200')
 
 @app.route('/vote', methods = ['GET'])
 def vote():
     return render_template('vote.html')
+
+@app.route("/uploadImage", methods=["POST"])
+def upload_image():
+    if request.files:
+        image = request.files["image"]
+        print(request.form['comment'])
+        image.save(os.path.join(app.root_path, 'static', 'img', image.filename))
+
+        print("Image saved")
+
+        return redirect('/')
+    return redirect(request.url)
--- app/static/javascript/main.js
+++ app/static/javascript/main.js
@@ -1,23 +1,3 @@
-$("#submitButton").click(function(){
-    var objectData =
-         {
-             data1: $("#data1").val(),
-             data2: $("#data2").val()
-         };
-
-    var objectDataString = JSON.stringify(objectData);
-    $.ajax({
-        url: "/challenge",
-        method:'POST',
-        dataType: 'json',
-        processData: false,
-        contentType: 'application/json',
-        data: objectDataString,
-    })
-    .done(function( msg ) {
-        alert( "Data Saved: " + msg );
-    });
-});
 
 function hoverBackArrow(element) {
   element.setAttribute('src', '/static/img/leftArrowFull.svg');
